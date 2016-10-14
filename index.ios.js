a51cdcd7674e114d531ef05f9b9eefd646feec2e
flyingace <dc@davidcameron.biz>
2016-10-13 23:31:39 -0400

    Files updated after updating React-Native with latest build version

--- index.ios.js
+++ index.ios.js
@@ -6,32 +6,33 @@ var {
     StatusBar,
     StyleSheet,
     View
-    } = require('react-native');
+} = require('react-native');
 const SideMenu = require('react-native-side-menu');
 const Application = require('./components/Application');
 
 var PantsWatch = React.createClass({
 
-    componentWillMount: function () {
-        StatusBar.setHidden(true);
-    },
 
-    render: function () {
-        return (
-                <Application />
-        );
-    }
+  componentWillMount: function () {
+    StatusBar.setHidden(true);
+  },
+
+  render: function () {
+    return (
+        <Application />
+    );
+  }
 });
 
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
+  }
 });
 
 AppRegistry.registerComponent('PantsWatch', () => PantsWatch);
